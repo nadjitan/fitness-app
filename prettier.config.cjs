31554b62ad55c58b142a87c1dead7df88dad0dbf
nadjitan <nadjitan@yahoo.com.ph>
2023-09-20 00:57:02 +0800

    project init

---
+++ prettier.config.cjs
@@ -0,0 +1,47 @@
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
+const config = {
+  plugins: [
+    'prettier-plugin-astro',
+    "prettier-plugin-tailwindcss",
+    // https://github.com/IanVS/prettier-plugin-sort-imports#install
+    "@ianvs/prettier-plugin-sort-imports"
+  ],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  semi: false,
+  tabWidth: 2,
+  singleQuote: false,
+  trailingComma: "none",
+  importOrder: [
+    "^astro",
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^types$",
+    "^@/store/(.*)$",
+    "^@/utils/(.*)$",
+    "^@/types/(.*)$",
+    "^@/config/(.*)$",
+    "^@/lib/(.*)$",
+    "^@/hooks/(.*)$",
+    "",
+    "^@/components/ui/(.*)$",
+    "^@/components/(.*)$",
+    "",
+    "^@/styles/(.*)$",
+    "^@/app/(.*)$",
+    "",
+    "^[./]"
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"]
+}
+
+module.exports = config
